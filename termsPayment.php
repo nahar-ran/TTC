8489044e76bb5229b8b39aab1c8b723b6d0a2c27
nahar-ran <nahar@miriraminfluewre.com>
2018-06-13 11:27:44 +0300

    update to latest files

---
+++ termsPayment.php
@@ -0,0 +1,24 @@
+These Terms of Payment must be read together with the Terms and Conditions of Business. The Terms and Conditions of Business apply to any agreement between Top Ten Coin and the Client generally, including for the avoidance of doubt the jurisdiction and governing law clause.
+
+PAYMENT COLLECTIONS AND FEES
+All credit and debit card payments for Top Ten Coin products and services are collected by RSK Ltd .
+
+Fees shall be payable as per invoices issued by Top Ten Coin to the Client. Should the Client fails to settle any invoice within a period as shall be specified in the invoice, Top Ten Coin reserve the right not to provide any service and/or to discontinue the provision of services, and shall not be responsible for any costs, fees, duties or taxes owed by the Client to any agent and/or government authority in any jurisdiction, any fines or fees incurred by the Client as a result of such withdrawal of services, nor for any consequential loss or claim made against the Client by any third party arising due to the non-payment. Should the Client maintains credit balance with Top Ten Coin and fails to settle any invoice within specified deadline, Top Ten Coin shall have the right to withdraw the respective due amount from such credit balance without Client’s prior consent. Fees for services and products offered by Top Ten Coin are published from time to time on the Top Ten Coin’s web sites, or may be notified to, or agreed with the Clients through direct communication. Top Ten Coin reserves the right to, if in our reasonable opinion, the economic conditions so necessitate, increase the fees without obligation to inform, or seek prior approval from, the Client.
+
+The price for any goods or services any sales tax, unless specified otherwise. The total purchase price, including sales tax, if any, will be displayed in the Client’s shopping cart prior to the payment for the order.
+
+Top Ten Coin’s fees for providing the Services will be based on the fee schedule in force at the time when the work is being performed. Top Ten Coin reserve the right periodically to update the prices on Top Ten Coin’s websites and to add to, amend, or withdraw the products and services that are offered, without prior notice. Top Ten Coin shall not be liable to anyone for withdrawing or amending any of the products we sell, or for refusing or failing to process an order.
+
+Top Ten Coin will not pay any interest on any money held on behalf of the Client or the Entity.
+
+REFUNDS AND CANCELLATIONS
+No refunds are given after Client’s order has been executed by Top Ten Coin partially or in full. No refunds will be made where Top Ten Coin is forced to refuse and/or cease to provide Services due to any breach by the Client of the
+
+warranties, obligations, and undertakings as specified in clauses 7-10 of this Terms and Conditions.
+
+Should Client purchases any of the services online via any of the Top Ten Coin’s websites and decides to cancel the order before Top Ten Coin start to execute such order, Top Ten Coin will refund all monies paid by the Client, except for an administration charge of US$ 200, which include merchant charges, order processing fees and other incidental expenses. The refund can be claimed within 180 days from the day of the payment. No refunds shall be given for the claims exceeding 180 days.
+
+Should Top Ten Coin cease to provide Services or should the Client advise Top Ten Coin that they no longer require the Entity, the Client must pay Top Ten Coin any already incurred fees, which remain outstanding and fees or costs, which may be incurred by Top Ten Coin in relation to the striking off, dissolution, liquidation or transfer of the Entity, including Top Ten Coin’s transfer, termination or exit fee.
+
+In the case of transfer of administration of the Entity to another service provider, Top Ten Coin will charge, and the Client will be obliged to pay, a transfer, or termination, or exit fee, or company release fee in accordance with the fee schedule valid on the date of the transfer, along with all outstanding fees incurred and being owed by the Client to Top Ten Coin up to the date of such transfer.
+
